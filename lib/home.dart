022199b88ec0e4699ffb39f3a2d7398b48ed64a7
Mustafa Özcan <mustafa452017@gmail.com>
2023-06-19 22:10:11 +0300

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final WebViewController _controller = WebViewController();
+  final String url = 'https://www.google.com.tr/';
+  @override
+  void initState() {
+    super.initState();
+    _controller.loadRequest(Uri.parse(url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+      ),
+      body: WebViewWidget(
+        controller: _controller,
+      ),
+    );
+  }
+}
